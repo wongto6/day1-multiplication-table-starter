04272417fafb3be0c26de1f663035313dbd0efc6
WONGTO6 <wongto6@oocl.com>
2024-11-18 17:44:40 +0800

    feat: finish validateStartEnd

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -19,5 +19,7 @@ public class MultiplicationTableBuilder {
         return multiplicationTable;
     }
 
-
+    public boolean validateStartEnd(int startInt, int endInt) {
+        return (startInt > 1 && startInt <= 1000) && (endInt > 1 && endInt <= 1000);
+    }
 }
